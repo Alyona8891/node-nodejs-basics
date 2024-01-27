63576a149dec2f281b2e6d41ac03a0a7877dd355
Alyona <besssta888@mail.ru>
2024-01-27 19:57:10 +0300

    feat: add solution to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const sourcePath = 'files';
+const destinationPath = 'files_copy';
+
 const copy = async () => {
-    // Write your code here 
+  fs.access(destinationPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      fsPromises
+        .cp(sourcePath, destinationPath, { recursive: true })
+        .then(() => console.log('The files were copied!'))
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await copy();
