5dac342f3830755802b9fdef2d28e050edf74730
Alyona <besssta888@mail.ru>
2024-01-27 20:48:55 +0300

    feat: add solutio to read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const path = 'files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fsPromises
+        .readFile(path, 'utf-8')
+        .then((data) => console.log(data))
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  });
 };
 
 await read();
