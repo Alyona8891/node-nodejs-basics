dd43552018824cf4d6c13b48e721c121a51149be
Alyona <besssta888@mail.ru>
2024-01-27 20:44:16 +0300

    feat: add solution to list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const path = 'files';
+
 const list = async () => {
-    // Write your code here 
+    fs.access(path, fs.constants.F_OK, (err) => {
+        if (err) {
+          throw new Error('FS operation failed');
+        } else {
+          fsPromises
+            .readdir(path)
+            .then((data) => console.log(data))
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+      });
 };
 
 await list();
