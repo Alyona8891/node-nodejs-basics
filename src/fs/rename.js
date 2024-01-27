c0acd10eb6d89d1e36630b8865a7c197fb34ef71
Alyona <besssta888@mail.ru>
2024-01-27 20:32:56 +0300

    feat: add solution to rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const oldPath = 'files/wrongFilename.txt';
+const newPath = 'files/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+  fs.access(oldPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fs.access(newPath, fs.constants.F_OK, (error) => {
+        if (error) {
+          fsPromises
+            .rename(oldPath, newPath)
+            .then(() => console.log('The file was renamed!'))
+            .catch((err) => {
+              throw new Error('FS operation failed');
+            });
+        } else {
+          throw new Error('FS operation failed');
+        }
+      });
+    }
+  });
 };
 
 await rename();
