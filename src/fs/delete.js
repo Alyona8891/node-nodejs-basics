e0c8304272b90bf8818b5e5cd181d424de5179ba
Alyona <besssta888@mail.ru>
2024-01-27 20:39:01 +0300

    feat: add solution to delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const path = 'files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fsPromises
+        .unlink(path)
+        .then(() => console.log('The file was removed!'))
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  });
 };
 
 await remove();
