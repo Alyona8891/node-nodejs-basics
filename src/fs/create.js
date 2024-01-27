18906649f07a52fbc8b930332ae77d3a86381868
Alyona <besssta888@mail.ru>
2024-01-27 18:31:58 +0300

    feat: add solution to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+const fsPromises = fs.promises;
+
+const text = 'I am fresh and young';
+const path = 'files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if(err) {
+      fsPromises.writeFile(path, text)
+        .then(() => console.log('The file was created!'))
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  })
 };
 
 await create();
