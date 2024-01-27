491681383cc5bb95f5115008e3b41db37318a862
Alyona <besssta888@mail.ru>
2024-01-27 20:45:38 +0300

    refactor: refactor list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -4,18 +4,18 @@ const fsPromises = fs.promises;
 const path = 'files';
 
 const list = async () => {
-    fs.access(path, fs.constants.F_OK, (err) => {
-        if (err) {
-          throw new Error('FS operation failed');
-        } else {
-          fsPromises
-            .readdir(path)
-            .then((data) => console.log(data))
-            .catch((err) => {
-              console.error(err);
-            });
-        }
-      });
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      fsPromises
+        .readdir(path)
+        .then((data) => console.log(data))
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  });
 };
 
 await list();
