b6094f0a78d8fb847ac7745b36a71275bfd9f254
Alyona <besssta888@mail.ru>
2024-01-30 01:21:41 +0300

    refactor: refactor cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,13 @@
 import { spawn } from 'node:child_process';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files', 'script.js');
 
 const spawnChildProcess = async (args) => {
-  const childProcess = spawn('node', ['./src/cp/files/script.js', ...args]);
+  const childProcess = spawn('node', [filePath, ...args]);
 
   process.stdin.pipe(childProcess.stdin);
   childProcess.stdout.pipe(process.stdout);
