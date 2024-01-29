c531e477d5b4882e40249a055ccdfc53fbe84979
Alyona <besssta888@mail.ru>
2024-01-29 11:51:41 +0300

    feat: add solution to read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream('files/fileToread.txt', 'utf-8');
+  readStream.on('data', (data) => console.log(data));
+  readStream.on('error', (err) => console.error(err));
 };
 
 await read();
