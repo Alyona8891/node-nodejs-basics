425f723ff7e154b447bb9859ee3d577b0ebba6a4
Alyona <besssta888@mail.ru>
2024-01-30 01:45:25 +0300

    refactor: refactor list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,15 +1,20 @@
 import fs from 'node:fs';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files');
+
 const fsPromises = fs.promises;
 
-const path = 'files';
-
 const list = async () => {
-  fs.access(path, fs.constants.F_OK, (err) => {
+  fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       throw new Error('FS operation failed');
     } else {
       fsPromises
-        .readdir(path)
+        .readdir(filePath)
         .then((data) => console.log(data))
         .catch((err) => {
           console.error(err);
