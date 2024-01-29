f0e2fad6c6e2701d265b4e625af17542a27c5900
Alyona <besssta888@mail.ru>
2024-01-30 01:39:14 +0300

    refactor: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,14 +1,20 @@
 import fs from 'node:fs';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const fsPromises = fs.promises;
 
-const text = 'I am fresh and young';
-const path = 'files/fresh.txt';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files', 'fresh.txt');
+
+const TEXT = 'I am fresh and young';
 
 const create = async () => {
-  fs.access(path, fs.constants.F_OK, (err) => {
+  fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       fsPromises
-        .writeFile(path, text)
+        .writeFile(filePath, TEXT)
         .then(() => console.log('The file was created!'))
         .catch((err) => {
           console.error(err);
