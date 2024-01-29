810f44ec14a4b19ec727f4d74eb317334216b236
Alyona <besssta888@mail.ru>
2024-01-29 16:02:58 +0300

    feat: add solution to decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  const source = createReadStream('files/archive.rar');
+  const destination = createWriteStream('files/fileToCompress.txt');
+
+  pipeline(source, unzip, destination, (err) => {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
