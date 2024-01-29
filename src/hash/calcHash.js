f5339c75752bff3beca5a09ee18f9cdabe791b9a
Alyona <besssta888@mail.ru>
2024-01-29 11:04:28 +0300

    feat: add solution to calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = createReadStream('files/fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  input.on('readable', () => {
+    const data = input.read();
+    data ? hash.update(data) : console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
