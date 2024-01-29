414cb6a61f8fdcc5e35b75b155ad0717d0b02d8c
Alyona <besssta888@mail.ru>
2024-01-29 15:32:45 +0300

    feat: add solution to transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, _, callback) {
+      this.push(chunk.toString().split('').reverse().join('') + '\n');
+      callback();
+    },
+  });
+
+  stdin.pipe(reverseStream).pipe(stdout);
 };
 
 await transform();
