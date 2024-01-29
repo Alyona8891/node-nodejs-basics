01ab5f4eca11d72d935003046b97b64f4741ca8d
Alyona <besssta888@mail.ru>
2024-01-30 01:57:04 +0300

    refactor: refactor streams files

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,8 +1,14 @@
 import fs from 'node:fs';
 import { stdout } from 'node:process';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
 
 const read = async () => {
-  const readStream = fs.createReadStream('files/fileToRead.txt', 'utf-8');
+  const readStream = fs.createReadStream(filePath, 'utf-8');
   readStream.on('data', (data) => stdout.write(data));
   readStream.on('error', (err) => console.error(err));
 };
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,8 +1,14 @@
 import fs from 'node:fs';
-import { stdout, stdin } from 'node:process';
+import { stdin } from 'node:process';
+import path, { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
 
 const write = async () => {
-  const writeStream = fs.createWriteStream('files/fileToWrite.txt');
+  const writeStream = fs.createWriteStream(filePath);
   stdin.on('data', (data) => {
     writeStream.write(data);
   });
