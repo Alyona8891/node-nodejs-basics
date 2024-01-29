24fcfc3682a86a26d6161846aa2a2d428e395c39
Alyona <besssta888@mail.ru>
2024-01-30 00:10:13 +0300

    feat: add solution to main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const INCREMENT = 10;
+
+const defineCPUCores = () => {
+  const cpuCores = os.cpus();
+  return cpuCores.length;
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const cores = defineCPUCores();
+  const workersArr = [];
+
+
+  for (let i = 1; i <= cores; i += 1) {
+    workersArr.push(new Worker('./src/wt/worker.js', { workerData: i + INCREMENT }));
+  }
+
+  Promise.allSettled(
+    workersArr.map((worker) => {
+      return new Promise((res, rej) => {
+        worker.on('message', (message) => {
+            res({ status: 'resolved', data: message });
+        });
+        worker.on('error', () => {
+            rej({ status: 'error', data: null });
+        });
+      });
+    })
+  ).then((results) => {
+    console.log(results.map(result => result.value));
+  });
 };
 
 await performCalculations();
