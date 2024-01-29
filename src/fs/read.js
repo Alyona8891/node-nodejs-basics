4640627866ddcdd8fdeaabd21b1c84d616bdf935
Alyona <besssta888@mail.ru>
2024-01-30 01:47:30 +0300

    refactor: refactor read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,15 +1,21 @@
 import fs from 'node:fs';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const fsPromises = fs.promises;
 
-const path = 'files/fileToRead.txt';
 
 const read = async () => {
-  fs.access(path, fs.constants.F_OK, (err) => {
+  fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
       throw new Error('FS operation failed');
     } else {
       fsPromises
-        .readFile(path, 'utf-8')
+        .readFile(filePath, 'utf-8')
         .then((data) => console.log(data))
         .catch((err) => {
           console.error(err);
