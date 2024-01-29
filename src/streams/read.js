c0f48c009b5d440b0fe82f1101fcf7eb05fdcd51
Alyona <besssta888@mail.ru>
2024-01-29 12:54:55 +0300

    refactor: refactor read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,8 +1,9 @@
 import fs from 'node:fs';
+import { stdout } from 'node:process';
 
 const read = async () => {
-  const readStream = fs.createReadStream('files/fileToread.txt', 'utf-8');
-  readStream.on('data', (data) => console.log(data));
+  const readStream = fs.createReadStream('files/fileToRead.txt', 'utf-8');
+  readStream.on('data', (data) => stdout.write(data));
   readStream.on('error', (err) => console.error(err));
 };
 
