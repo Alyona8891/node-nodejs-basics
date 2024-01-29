cb33a56e539f79535bd664981adfa463ee36beaa
Alyona <besssta888@mail.ru>
2024-01-30 01:59:15 +0300

    refactor: refactor main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,11 @@
 import { Worker } from 'node:worker_threads';
 import os from 'node:os';
+import path, {join} from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = join(__dirname, 'worker.js');
 
 const INCREMENT = 10;
 
@@ -14,7 +20,7 @@ const performCalculations = async () => {
 
   for (let i = 1; i <= cores; i += 1) {
     workersArr.push(
-      new Worker('./src/wt/worker.js', { workerData: i + INCREMENT })
+      new Worker(filePath, { workerData: i + INCREMENT })
     );
   }
 
